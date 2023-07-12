0762011b5b96935b55f14c1d6e1bb58d1878319d
thePythoninja <olaoluwathompson8@gmail.com>
2023-07-12 15:18:55 +0100

    Trillo App

---
+++ test.js
@@ -0,0 +1,10 @@
+// const obj = {
+//   name: "Bola Ahmed Tinubu",
+//   isWin: true,
+// };
+
+// var obj2 = new obj();
+// obj2.isWin = false;
+
+// console.log(obj.isWin);
+// console.log(obj2.isWin);
